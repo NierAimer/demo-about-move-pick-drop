ca66445863f66e962ec10f261d6a69204a7e6d6c
AdaAimer <adanieraimer@outlook.com>
2022-11-11 14:08:46 +0800

    first commit

---
+++ Source/FactorialProject/FactorialProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FactorialProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FactorialProject, "FactorialProject" );
---
+++ Source/FactorialProject/FactorialProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FactorialProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FACTORIALPROJECT_API AFactorialProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
